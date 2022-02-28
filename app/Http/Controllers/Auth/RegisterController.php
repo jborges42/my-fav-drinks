d94147235c229b038ffca9bdef5618fb8b8fc5d8
jborges42 <joao.borges@redacaonline.com.br>
2022-02-28 14:04:45 -0300

    add document to register form

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -67,6 +67,7 @@ class RegisterController extends Controller
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
+            'document' => $data['document'],
             'password' => Hash::make($data['password']),
         ]);
     }
