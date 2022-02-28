f3e778c7f007a8609bfde0edc08833256a30d0a2
jborges42 <joao.borges@redacaonline.com.br>
2022-02-28 14:19:39 -0300

    add drink resources

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DrinkController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 
 Auth::routes();
 
+Route::middleware(['auth'])->group(function () {
+    Route::resource('drinks', DrinkController::class);
+});
